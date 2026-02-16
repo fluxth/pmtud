1af65f2416a954c48d024b5bd082b585101923f3
Thitat Auareesuksakul <flux@thitat.net>
2026-02-16 21:17:32 +0900

    feat: macos ipv4 pmtud via icmp

---
+++ src/arch/macos/icmpv6.rs
@@ -0,0 +1,5 @@
+use std::net::Ipv6Addr;
+
+pub(crate) fn icmpv6_pmtud(_target: Ipv6Addr) {
+    todo!()
+}
--- src/arch/macos/ipv6.rs
+++
@@ -1,5 +0,0 @@
-use std::net::Ipv6Addr;
-
-pub(crate) fn ipv6_pmtud(target: Ipv6Addr) {
-    todo!()
-}
