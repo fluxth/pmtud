1a89a6258a1b88b7fbb0dd04e3542c60698b8286
Thitat Auareesuksakul <flux@thitat.net>
2026-02-17 22:31:01 +0900

    feat: macos ipv6 pmtud via icmpv6

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -6,7 +6,7 @@ mod macos;
 pub(crate) fn icmpv6_pmtud(target: Ipv6Addr) {
     #[cfg(target_os = "macos")]
     {
-        macos::icmpv6::icmpv6_pmtud(target)
+        macos::icmpv6::icmpv6_pmtud(target).unwrap()
     }
 
     #[cfg(not(target_os = "macos"))]
